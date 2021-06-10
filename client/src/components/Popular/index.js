a2a5387b2e71b7fffce6bb4b11c571bbd703043e
Lim95 <sungjoon.lim@gmail.com>
2021-06-10 11:31:07 -0700

    homepage on progress

--- client/src/components/Popular/index.js
+++ client/src/components/Popular/index.js
@@ -1,12 +1,26 @@
 import React, { useCallback } from "react";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
+import data from "../../pages/data";
 function Popular() {
 
-      return (
-         0
+    return ( 
+        <div className="popular">
+            <h2>Most Popular</h2>
+            <div className="popular-container">
 
-  );
+                {data.products.map((product) => (
+                <div className="card"key={product._id}>
+
+                    <img className="product-img"src={product.image} alt={product.name}/>
+                    <p className="product-name">{product.name}</p>
+                    <p className="product-price">{product.price}</p>
+
+                </div>
+                ))}
+            </div>
+        </div>
+    );
 }
 
 export default Popular;
